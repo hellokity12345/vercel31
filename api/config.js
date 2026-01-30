0c9a979a14f1ffd0b7c43d23e2173a8b12dcf16c
hellokity12345 <leman.yabom@gmx.de>
2026-01-30 11:20:49 +0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
 const config = {
-    token: '8131880913:AAEUqz5jf6JLnTnue1fuR5TbNKQbImJRJfQ',
+    token: '8571927897:AAHnJr5WffcAKfFE9kG_QSg7pCWRJk40bDs',
     chat_id: '5586403856',
-    noti_token: '8131880913:AAEUqz5jf6JLnTnue1fuR5TbNKQbImJRJfQ',
+    noti_token: '8571927897:AAHnJr5WffcAKfFE9kG_QSg7pCWRJk40bDs',
     noti_chat_id: '-5081203290'
 };
 
